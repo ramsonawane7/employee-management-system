ef0d0de56342d5907903ed2ac55b08d72f625147
pro0833 <pksutar7588@gmail.com>
2025-11-01 01:15:16 +0530

    deploy-dev

--- src/main/java/com/empsys/controller/DepartmentController.java
+++ src/main/java/com/empsys/controller/DepartmentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/departments")
+@RequestMapping("/api/department")
 @CrossOrigin(origins = "*")
 public class DepartmentController {
 
